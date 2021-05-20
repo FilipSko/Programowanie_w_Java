7eda145f9cd329bca04b1618500966885118dccd
FilipSko <1filip2@wp.pl>
2021-05-20 18:16:18 +0200

    Zadanie 2.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,15 @@ public class Main {
         Human me = new Human();
         me.firstName = "Filip";
         me.lastName = "Skośkiewicz";
-        me.pet = new Animal("Felis",2.0);
+        me.pet = new Animal("Felis", 2.0);
+        me.car = new Car("Punto II", "Fiat", "Red", 70);
 
         me.pet.feed();
         me.pet.takeForAWalk();
         me.pet.takeForAWalk();
         me.pet.takeForAWalk();
+
+        System.out.println(me.car.model);
 
     }
 }
