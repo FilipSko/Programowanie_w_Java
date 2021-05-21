1bc2fe1684a2dfcf2120e299d42cfc10af61e487
FilipSko <1filip2@wp.pl>
2021-05-21 14:18:56 +0200

    Zadanie 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 import com.company.creatures.FarmAnimal;
 import com.company.creatures.Human;
 import com.company.creatures.Pet;
+import com.company.devices.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -13,12 +17,28 @@ public class Main {
         me.lastName = "Skośkiewicz";
         Pet dog = new Pet("dogos", 30.0, "Fado", null);
         FarmAnimal cow = new FarmAnimal("meat", 120.0, "Milka", null);
-        cow.takeForAWalk();
-        me.pet = dog;
-        cow.feed();
-        dog.feed();
-        dog.takeForAWalk();
-        cow.beEaten();
+
+        Car fiat = new Disel("Fiat", "Punto", 2003);
+        fiat.refuel();
+
+        Car volvo = new Electric("Volvo", "Recharge", 2020);
+        volvo.refuel();
+
+        Car BMW = new LPG("BMW", "3", 1998);
+        BMW.refuel();
+
+        Phone sony = new Phone("Sony", "Xperia", 4.7, "Android", 2013);
+        sony.installAnApp("Pokemongo");
+        sony.installAnApp("Messenger", "2.4.1.");
+        sony.installAnApp("Signal", "3.1.1.", "download.signal.com");
+
+        List<String> appNames = new ArrayList<>();
+        appNames.add("Google");
+        appNames.add("Yazio");
+        appNames.add("Facebook");
+
+        sony.installAnApp(appNames);
+
 
     }
 }
--- src/com/company/devices/Phone.java
+++ src/com/company/devices/Phone.java
@@ -3,9 +3,16 @@ package com.company.devices;
 import com.company.creatures.Human;
 import com.company.creatures.Sellable;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 public class Phone extends Device implements Sellable {
     final Double screenSize;
     final String operatingSystem;
+    static final String DEFAULT_SERVER_ADDRESS = "100.0.0.1";
+    static final String DEFAULT_SERVER_PROTOCOL = "http";
+    static final String DEFAULT_VERSION_NAME = "latest";
 
     public Phone(String producer, String model, Double screenSize, String operatingSystem, int yearOfProduction) {
         super(model, producer, yearOfProduction);
@@ -36,5 +43,44 @@ public class Phone extends Device implements Sellable {
         buyer.mobilePhone = seller.mobilePhone;
         seller.mobilePhone = null;
         System.out.println("Trasnakcja udana. Telefon został kupiony za " + price);
+    }
+
+    public void installAnApp(String appName) {
+        this.installAnApp(appName, DEFAULT_VERSION_NAME);
+    }
+
+    public void installAnApp(String appName, String version) {
+        this.installAnApp(appName, version, DEFAULT_SERVER_ADDRESS);
+    }
+
+    public void installAnApp(String appName, String version, String server) {
+        URL url = null;
+        try {
+            url = new URL(DEFAULT_SERVER_PROTOCOL, server, appName + version);
+            this.installAnApp(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void installAnApp(URL appURL) {
+        System.out.println("Pobieranie aplikacji " + appURL.getFile() + " ze strony " + appURL.getHost());
+        System.out.println("Sprawdzanie, czy aplikacja jest płatna");
+        System.out.println("Sprawdzanie, czy masz pieniądze");
+        System.out.println("Sprawdzanie, czy masz miejsce");
+        System.out.println("Jeśli aplikacja jest bezpłatna lub zapłaciłeś, rozpakowuję.");
+        System.out.println("Instalowanie aplikacji " + appURL.getFile());
+    }
+
+    public void installAnApp(List<String> appNames) {
+        String[] lista = new String[appNames.size()];
+        lista = appNames.toArray(lista);
+        this.installAnApp(lista);
+    }
+
+    public void installAnApp(String[] appNames) {
+        for (String appName : appNames) {
+            this.installAnApp(appName);
+        }
     }
 }
